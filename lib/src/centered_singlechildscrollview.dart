a49a00eef0e3af08f0da71c99f1ef74a7d48ad9f
ilham-asgarli <ilhamasgarli19@gmail.com>
2022-09-27 04:32:00 +0300

    Initial commit

--- lib/src/centered_singlechildscrollview.dart
+++ lib/src/centered_singlechildscrollview.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
 
 class CenteredSingleChildScrollView extends StatelessWidget {
-  final List<Widget>? children;
-  final MainAxisAlignment? mainAxisAlignment;
+  final ScrollController? scrollController;
+  final List<Widget> children;
+  final MainAxisAlignment mainAxisAlignment;
+  final Axis axis;
+  final bool reverse;
+  final bool? primary;
+  final EdgeInsetsGeometry? padding;
 
-  const CenteredSingleChildScrollView(
-      {this.children, this.mainAxisAlignment, Key? key})
-      : super(key: key);
+  const CenteredSingleChildScrollView({
+    this.scrollController,
+    this.children = const [],
+    this.mainAxisAlignment = MainAxisAlignment.center,
+    this.axis = Axis.vertical,
+    this.reverse = false,
+    this.padding,
+    this.primary,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
         return SingleChildScrollView(
+          primary: primary,
+          padding: padding,
+          controller: scrollController,
+          reverse: reverse,
+          scrollDirection: axis,
           physics: const ClampingScrollPhysics(),
           child: ConstrainedBox(
             constraints: BoxConstraints(
-                minWidth: constraints.maxWidth,
-                minHeight: constraints.maxHeight),
+              minWidth: constraints.maxWidth,
+              minHeight: constraints.maxHeight,
+            ),
             child: IntrinsicHeight(
               child: Column(
-                mainAxisAlignment:
-                    mainAxisAlignment ?? MainAxisAlignment.center,
-                children: children ?? [],
+                mainAxisAlignment: mainAxisAlignment,
+                children: children,
               ),
             ),
           ),
